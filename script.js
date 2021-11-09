fdc7dfdd5831f7ac2af28daee5d503a2e83b4f1a
lucasvidela94 <69171039+lucasvidela94@users.noreply.github.com>
2021-11-09 00:34:34 -0300

    cambio de proxy url

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('Hola mundo!');
+//Sacar cita de API
+async function getQuote() {
+    const proxyUrl = 'https://whispering-tor-04671.herokuapp.com/'
+    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+    try {
+        const response = await fetch(proxyUrl + apiUrl);
+        const data = await response.json();
+        
+    } catch(error){
+        console.log('error,no hay cita', error);
+    }
+
+}
+
+//Llamada a funcion
+
+getQuote();

e42eda197397ff74a9a9c70d05ccba979c513b92
lucasvidela94 <69171039+lucasvidela94@users.noreply.github.com>
2021-12-06 15:28:52 -0300

    Añadido funcion loading que muestra simbolo de carga y funcion complete que desaparece el mismo simbolo al obtener cita

--- script.js
+++ script.js
@@ -1,22 +1,41 @@
-const quoteContainer = document.getElementById("contenedor-citas");
-const quoteText = document.getElementById("cita");
-const authorText = document.getElementById("autor");
-const twitterButton = document.getElementById("twitter");
-const newQuoteButton = document.getElementById("nueva-cita");
+const quoteContainer = document.getElementById('contenedor-citas');
+const quoteText = document.getElementById('cita');
+const authorText = document.getElementById('autor');
+const twitterButton = document.getElementById('twitter');
+const newQuoteButton = document.getElementById('nueva-cita');
+const loader = document.getElementById('loader')
 
+
+//Mostrar el simbolo de cargar mientras espera a la devolucion de la cita
+
+function loading(){
+    loader.hidden= false;
+    quoteContainer.hidden = true;
+}
+
+//Esconder simbolo de cargar
+
+function complete(){
+    if(!loader.hidden){
+        quoteContainer.hidden=false;
+        loader.hidden =true;
+    }
+}
 
 //Sacar cita de API -- Gettin quote from quote API
 async function getQuote() {
+    loading();
+
     const proxyUrl = 'https://whispering-tor-04671.herokuapp.com/'
     const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
     try {
         const response = await fetch(proxyUrl + apiUrl);
         const data = await response.json();
-        //Si el autor es un string vacio se le atribuye "Unknown"
+        //Si el autor es un string vacio se le atribuye 'Unknown'
         if (data.quoteAuthor === ''){
             authorText.innerText = 'Unknown';
         } 
-        else {
+        else{
             authorText.innerText = data.quoteAuthor;
         }
 
@@ -30,6 +49,9 @@ async function getQuote() {
         
         quoteText.innerText = data.quoteText;
         
+        //Proceso completado,se muestra la cita
+        complete(); 
+
     } catch (error) {
         getQuote();
     }
